0a2253f2e966d5593365d5073228d33edb2bd81f
Raghad-Moqady <raghadmoqady@gmail.com>
2025-10-30 15:50:36 +0200

    edit contact inputs style

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -10,19 +10,19 @@ function Contact(){
             <Title title="Contact Me"/>
             <form className='w-50 m-auto'>
                   <div className="form-floating mb-3">
-                    <input type="text" className="form-control" id="floatingInput" placeholder=" "/>
+                    <input type="text" className="form-control rounded-5" id="floatingInput" placeholder=" "/>
                     <label htmlFor="floatingInput">Full Name</label>
                     </div>  
                     <div className="form-floating mb-3">
-                    <input type="email" className="form-control" id="floatingInput" placeholder=""/>
+                    <input type="email" className="form-control rounded-5" id="floatingInput" placeholder=""/>
                     <label htmlFor="floatingInput">Email address</label>
                     </div>         
                     <div className="form-floating mb-3">
-                    <input type="text" className="form-control" id="floatingInput" placeholder=""/>
+                    <input type="text" className="form-control rounded-5" id="floatingInput" placeholder=""/>
                     <label htmlFor="floatingInput">Phone Number</label>
                     </div>         
                     <div className="form-floating">
-                    <textarea className="form-control h-25 " placeholder="" id="floatingTextarea2" ></textarea>
+                    <textarea className="form-control h-25 rounded-5 " placeholder="" id="floatingTextarea2" ></textarea>
                     <label htmlFor="floatingTextarea2">Message</label>
                     </div>  
                     <div className=' text-center'>
